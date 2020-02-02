7e16245a30bc16a84b27a77dbef3e9ffe5ffb461
GoudekettingRM <r.goudeketting@gmail.com>
2020-02-02 21:55:47 +0100

    change character card to bootstrap card

--- src/components/Cards/CharacterCard.jsx
+++ src/components/Cards/CharacterCard.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./card.css";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { LinkContainer } from "react-router-bootstrap";
 
 export default function CharacterCard(props) {
   const { image, name, id } = props.data;
   return (
-    <div className="card">
-      <img className="cardImg" src={image} alt={name} />
-      <h5>{name}</h5>
-      <Link className="moreInfo" to={`/characters/${id}`}>
-        More about {name}
-      </Link>
-    </div>
+    <Card style={{ width: "12rem", margin: "0 auto" }}>
+      <Card.Img variant="top" src={image} />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <LinkContainer to={`/characters/${id}`}>
+          <Button variant="outline-secondary">More about {name}</Button>
+        </LinkContainer>
+      </Card.Body>
+    </Card>
   );
 }
