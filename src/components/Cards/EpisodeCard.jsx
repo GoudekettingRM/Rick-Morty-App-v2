881ccf277a371ccbb65c43b511b106d8c51873f6
GoudekettingRM <r.goudeketting@gmail.com>
2020-02-02 22:16:57 +0100

    episode card styling

--- src/components/Cards/EpisodeCard.jsx
+++ src/components/Cards/EpisodeCard.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./card.css";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { LinkContainer } from "react-router-bootstrap";
 
 export default function EpisodeCard(props) {
   const { episode, name, id, air_date } = props.data;
   return (
-    <div className="card">
-      <h5>{name}</h5>
-      <p>
-        {episode} <br /> {air_date}
-      </p>
-      <Link className="moreInfo" to={`/episodes/${id}`}>
-        More about {episode}
-      </Link>
-    </div>
+    <Card className="epCard">
+      <Card.Body className="d-flex flex-column">
+        <Card.Title style={{ color: "maroon" }}>{name}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">{episode}</Card.Subtitle>
+        <Card.Text>Aired: {air_date}</Card.Text>
+        <LinkContainer to={`/episodes/${id}`}>
+          <Button className="mt-auto" variant="outline-secondary">
+            More about {name}
+          </Button>
+        </LinkContainer>
+      </Card.Body>
+    </Card>
   );
 }
