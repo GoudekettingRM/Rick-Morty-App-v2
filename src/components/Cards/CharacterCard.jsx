cdf66e973795d7113c25550d9290dca5fe40df2b
GoudekettingRM <r.goudeketting@gmail.com>
2020-02-02 22:04:44 +0100

    fix card styling

--- src/components/Cards/CharacterCard.jsx
+++ src/components/Cards/CharacterCard.jsx
@@ -8,12 +8,14 @@ import { LinkContainer } from "react-router-bootstrap";
 export default function CharacterCard(props) {
   const { image, name, id } = props.data;
   return (
-    <Card style={{ width: "12rem", margin: "0 auto" }}>
+    <Card className="card">
       <Card.Img variant="top" src={image} />
-      <Card.Body>
+      <Card.Body className="d-flex flex-column">
         <Card.Title>{name}</Card.Title>
         <LinkContainer to={`/characters/${id}`}>
-          <Button variant="outline-secondary">More about {name}</Button>
+          <Button className="mt-auto" variant="outline-secondary">
+            More about {name}
+          </Button>
         </LinkContainer>
       </Card.Body>
     </Card>
